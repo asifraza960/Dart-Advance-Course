89e457014747a4c553b4794edf12af8fb0de2b9f
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-07-07 14:35:07 +0500

    update

--- opp_in_dart/Encapsulation/example1.dart
+++ opp_in_dart/Encapsulation/example1.dart
@@ -1,6 +1,8 @@
+import 'dart:ffi';
+
 class Person{
-  String? _name;
-  int? _age;
+  String _name;
+  int _age;
 
 
    Person(this._name ,this._age);
@@ -8,12 +10,16 @@ class Person{
 
   //  make getter
 
-  String? get name  => _name;
-  int? get age => _age;
+  String get name  => _name;
+  int get age => _age;
 
-
+// make setter
   set setName(String name){
     _name = name;
+  }
+
+  void display(){
+    print(name);
   }
 
 }
--- opp_in_dart/Encapsulation/main.dart
+++ opp_in_dart/Encapsulation/main.dart
@@ -1,3 +1,14 @@
+import 'example1.dart';
+
 void main (){
+  Person person = Person('asli', 23);
+
+
+  person.display();
+      print(person.name);
+      print(person.age);
+      person.setName = "asif";
+      print(person.name);
   
+
 }
