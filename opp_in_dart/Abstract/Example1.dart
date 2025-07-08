c65733e25a5ccca14bb123468368d3f495c412ec
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-07-08 15:00:26 +0500

    update

---
+++ opp_in_dart/Abstract/Example1.dart
@@ -0,0 +1,27 @@
+abstract class BaseApiServices{
+
+void postApi(); 
+// mwthod 
+void getApi();
+
+}
+
+
+
+class NetworkApiServices extends BaseApiServices{
+
+
+  @override
+  void getApi() {
+    // TODO: implement getApi
+
+
+  }
+
+  @override
+  void postApi() {
+    // TODO: implement postApi
+
+    
+  }
+}
