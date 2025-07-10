d2c42ad0e8f6de95e2cdebe1b9dbc18e94e1c00c
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-07-10 22:15:43 +0500

    update

---
+++ opp_in_dart/Sealed/Pol/about_Polymorphism .dart
@@ -0,0 +1,4 @@
+// Polymorphism ka matlab:
+// "Poly" ka matlab hota hai "many" aur "morph" ka matlab hota hai "forms"
+// Toh Polymorphism = Many Forms
+// Yani ek hi naam ka function/method, lekin uska behavior alag ho sakta hai.
