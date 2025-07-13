7591a55765c87e1f1377b487daabc935876e2c6c
Asif Raza <148605750+asif-raza12345@users.noreply.github.com>
2025-07-13 20:53:20 +0500

    update

--- opp_in_dart/Sealed/Pol/about_Polymorphism .dart
+++ opp_in_dart/Sealed/Pol/about_Polymorphism .dart
@@ -4,6 +4,6 @@
 // Yani ek hi naam ka function/method, lekin uska behavior alag ho sakta hai.
 
 
-// 
+// defination
 // Polymorphism ka matlab hota hai ke aik hi function ya method different forms mein kaam kare — yani 
 //aik hi naam ka function mukhtalif behavior show kare depending on context.
