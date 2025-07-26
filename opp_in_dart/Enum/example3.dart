cf385c8a70a9e2f67d1255c3f215a90fd7bc8aa2
Asif Raza <razamakki335@gmail.com>
2025-07-26 09:04:37 -0700

    update

--- opp_in_dart/Enum/example3.dart
+++ opp_in_dart/Enum/example3.dart
@@ -1,18 +1,5 @@
-enum Status {
-  loading,
-  success,
-  error,
-}
+enum Status { loading, success, error }
+
 Status status = Status.loading;
-if (status == Status.success) {
-print("Success!");
-}
 
-
-enum Animal {
-  cat,
-  dog,
-  bird,
-  bear,
-}
-
+enum Animal { cat, dog, bird, bear, loin }
