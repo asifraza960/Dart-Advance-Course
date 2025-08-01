1f45f2498bcf3f8ff5d6f82165191598e25196d4
asifraza960 <razamakki335@gmail.com>
2025-08-01 07:51:09 +0500

    update

--- opp_in_dart/Enum/example3.dart
+++ opp_in_dart/Enum/example3.dart
@@ -1,18 +1,9 @@
-<<<<<<< HEAD
-enum Status {
-  loading,
-  success,
-  error,
+enum DaysOfWeek {
+  Monday,
+  Tuesday,
+  Wednesday,
+  Thursday,
+  Friday,
+  Saturday,
+  Sunday
 }
-Status status = Status.loading;
-
-if (status == Status.success) {
-print("Success!");
-}
-=======
-enum Status { loading, success, error }
-
-Status status = Status.loading;
-
-enum Animal { cat, dog, bird, bear, loin }
->>>>>>> cf385c8a70a9e2f67d1255c3f215a90fd7bc8aa2
